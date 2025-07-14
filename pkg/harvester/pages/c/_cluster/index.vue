a1f21735806e64188a79db21c5a8055e170a8eec
oshyuni93 <oshyuni@gmail.com>
2025-07-14 15:02:08 +0900

    v1.5.1 update

---
+++ pkg/harvester/pages/c/_cluster/index.vue
@@ -0,0 +1,29 @@
+<script>
+import { PRODUCT_NAME } from '../../../config/harvester';
+import { HCI } from '../../../types';
+
+export default {
+  beforeCreate() {
+    this.$router.replace({
+      name:   `${ PRODUCT_NAME }-c-cluster-resource`,
+      params: {
+        ...this.$router.currentRoute.params,
+        //product:  PRODUCT_NAME,
+        product:  `Infinitystack`,
+        resource: HCI.DASHBOARD
+      }
+    });
+  }
+};
+</script>
+
+<template>
+  <!--
+  <div>
+    This is harvester Dashboard.
+  </div>
+  -->
+  <div>
+    This is Infinitystack Dashboard.
+  </div>
+</template>
